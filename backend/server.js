3d39cba476ea4e0359a265afdb72fcbca9e65cde
Chetna Ghengare <chetnaghengare@gmail.com>
2024-09-25 11:19:06 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,29 @@ app.get('/' , (req , res) =>{
     res.send('Hello World!');
 })
 
+
+// get list of 5 jokes
+
+app.get('/jokes' , (req,resp)=>{
+    const jokes = [
+        {
+            id : 1 ,
+            title : 'A joke' ,
+            content : 'this is a joke' 
+        },
+        {
+            id : 1 ,
+            title : '2nd joke' ,
+            content : 'this  2nd is a joke' 
+        },
+        {
+            id : 1 ,
+            title : '3rd joke' ,
+            content : 'this 3rd is a joke' 
+        },
+    ];
+    resp.send(jokes);
+});
 const port =  process.env.PORT || 3000;
 
 app.listen(port , ()=>{
